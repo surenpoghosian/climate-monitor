717e8cda214fc7709f062f338a7d85a271482218
Suren Poghosyan <suren.poghosian.18@gmail.com>
2025-06-25 13:04:01 +0400

    feat: implement subfunctions for SerialManager

--- UI/src/DataManagers/SerialManager.ts
+++ UI/src/DataManagers/SerialManager.ts
@@ -1,5 +1,85 @@
+import { SerialPort } from "serialport";
+import parseSerialData from "../utils/parseSerialData";
+
+type DataCallback = (data: Buffer) => void;
+
+export enum BaudRate {
+  R110 = 110,
+  R300 = 300,
+  R600 = 600,
+  R1200 = 1200,
+  R2400 = 2400,
+  R4800 = 4800,
+  R9600 = 9600,
+  R14400 = 14400,
+  R19200 = 19200,
+  R28800 = 28800,
+  R38400 = 38400,
+  R57600 = 57600,
+  R115200 = 115200,
+  R128000 = 128000,
+  R256000 = 256000,
+}
 class SerialManager {
-  constructor() {
+  private currentPort: string;
+  private baudRate: number;
+  private portInstance: SerialPort | null = null;
+  private dataCallbacks: DataCallback[] = [];
+
+  constructor() {}
+
+  async getPortList() {
+    const portList = await SerialPort.list();
+    return portList;
+  }
+
+  setPort(port: string) {
+    this.currentPort = port;
+  }
+
+  setBaudRate(rate: BaudRate) {
+    this.baudRate = rate;
+  }
+
+  connect() {
+    if (!this.currentPort || !this.baudRate) {
+      throw new Error('Port and baud rate must be set before connecting');
+    }
+
+    this.portInstance = new SerialPort({
+      path: this.currentPort,
+      baudRate: this.baudRate,
+    });
+
+    this.portInstance.on('open', () => {
+      console.log(`connected to ${this.currentPort}, with baud rate ${this.baudRate}`);
+    });
+
+    this.portInstance.on('error', (err) => {
+      console.error('Serial error:', err.message);
+    });
+
+    this.portInstance.on('data', (data: Buffer) => {
+      console.log('Arduino says:', { type: typeof data, value: data });
+      this.dataCallbacks.forEach(callback => callback(data));
+      // console.log(parseSerialData(data as Buffer));
+    });
+  }
+
+  subscribeToUpdates(callback: DataCallback) {
+    this.dataCallbacks.push(callback);
+  }
+
+  disconnect() {
+    if (this.portInstance && this.portInstance.isOpen) {
+      this.portInstance.close((err) => {
+        if (err) {
+          console.error("Error while closing the port", err.message);
+        } else {
+          console.log('serial port closed');
+        }
+      });
+    }
   }
 }
 
--- UI/src/main.ts
+++ UI/src/main.ts
@@ -3,6 +3,7 @@ import { app, BrowserWindow } from 'electron';
 import * as path from 'path';
 import DataManager from './DataManagers/DataManager';
 import parseSerialData from './utils/parseSerialData';
+import SerialManager, { BaudRate } from './DataManagers/SerialManager';
 
 function createWindow() {
   const wndw = new BrowserWindow({
@@ -23,25 +24,24 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
 
-// const port = new SerialPort({
-//   path: '/dev/tty.usbmodem21201',
-//   baudRate: 115200
-// });
-
-// port.on('error', (err) => {
-//   console.error('Serial error:', err.message);
-// });
-
-// port.on('data', (data) => {
-//   console.log('Arduino says:', { type: typeof data, value: data });
-//   console.log(parseSerialData(data as Buffer));
-// });
-
 async function main () {
-  const serialPorts = await SerialPort.list();
+  const serialPorts = await SerialManager.getPortList();
   const portPaths = serialPorts.map(port => port.path);
 
-  DataManager.setAvailableSerialPorts(portPaths)
+  DataManager.setAvailableSerialPorts(portPaths);
+
+  const ports = DataManager.getAvailableSerialPorts();
+
+  SerialManager.setPort(ports[0]);
+  SerialManager.setBaudRate(BaudRate.R115200);
+  SerialManager.connect();
+
+
+  SerialManager.subscribeToUpdates((data) => {
+    console.log('Buffer data: ', data);
+    const parsed = parseSerialData(data);
+    console.log('Parsed data: ', parsed);
+  })
 }
 
 main().catch(console.error);
